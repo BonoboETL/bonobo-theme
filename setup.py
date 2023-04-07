a3f0d7f91ffda5b3e45721f9e74798c3f1811bd2
Andrew Mirsky <andrew@mirsky.net>
2023-04-07 18:26:56 -0400

    initial design commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from bonobo_theme import _version as version
+
+from setuptools import setup
+
+# README into long description
+# with codecs.open('README.rst', encoding='utf-8') as f:
+#     readme = f.read()
+
+setup(
+    name='bonobo-theme',
+    version=version.__version__,
+    description='Theme for Bonobo ETL',
+    long_description='Theme for Bonobo ETL',
+    author='Andrew Mirsky',
+    author_email='andrew@mirskytech.com',
+    url='https://github.com/bonoboetl/bonobo-theme',
+    packages=['bonoboetl-theme'],
+    include_package_data=True,
+    entry_points={
+        'sphinx.html_themes': [
+            'bonobo = bonobo-theme',
+        ]
+    },
+    classifiers=[
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+        'Programming Language :: Python',
+    ],
+)
